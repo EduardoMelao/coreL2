87c6f2d32231c7e0df849eaf0bda7bf52d748eff
EduardoMelao <eduardo.melao@engenharia.ufjf.br>
2019-11-27 11:04:49 -0300

    Added class L1L2Interface to manage information exchanged between MAC and PHY. Timeout unity changed to nanoseconds and timeout set to 1ns to increase performance.

--- src/CoreL2/MacController/MacCtHeader.h
+++ src/CoreL2/MacController/MacCtHeader.h
@@ -36,30 +36,11 @@ public:
      * @param _verbose Verbosity flag
      */
     MacCtHeader(bool _flagBS, bool _verbose);
-    
 
     /**
-     * @brief Builds a Control Header from a PDU received on decoding and makes possible the use of these informations
-     * @param _flagBS BS flag: true for BS; false for UE
-     * @param buffer Buffer containing PDU
-     * @param size Size in bytes of PDU
-     * @param _verbose Verbosity flag
-     */ 
-    MacCtHeader(bool _flagBS, char* buffer, int size, bool _verbose);
-
-    /**
-     * @brief Inserts a Control Header in PDU's encoding process
-     * @param buffer Buffer containing PDU to be encoded
-     * @param size Size of PDU in bytes
-     * @returns New PDU size after Header insertion
+     * @brief Gets the Control Header to be shared with PHY in PDU's encoding process
+     * @param buffer Buffer to store control information
+     * @returns Control data size in bytes
      */
-    ssize_t insertControlHeader(char* buffer, int size);
-
-    /**
-     * @brief Removes a Control Header in PDU's decoding process
-     * @param buffer Buf containing PDU to be decoded
-     * @param size Size of PDU in bytes
-     * @returns Size of decoded PDU
-     */
-    ssize_t removeControlHeader(char* buffer, int size);
+    ssize_t getControlData(char* buffer);
 };
