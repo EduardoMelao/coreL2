6256a1cc8ad24b4abd4ecfc56a51e87e763dda18
EduardoMelao <eduardo.melao@engenharia.ufjf.br>
2019-11-29 12:23:49 -0300

    Data control separation (#3)
    * Alterations in names and directories of some arquives.
    
    * Some errors because I did not run git fetch
    
    * Minor directory correction
    
    * Created classes TransmissionProtocol and ReceptionProtocol to act as brokers with transmission and reception of packets.
    
    * BugFix receiving PDU and verbose additions
    
    * Created ProtocolData to detach enqueueing Data SDUs from MacController. Needs testing
    
    * Fixed problem of one class declaration in another class
    
    * Added ProtocolControl. Removed procedures that control enqueueing SDUs from MacController. Splitted decoding procedures: control and data.
    
    * Bugfix: type unsigned int receiving negative values

---
+++ src/coreL2/TransmissionProtocol/TransmissionProtocol.cpp
@@ -0,0 +1,58 @@
+/* ***************************************/
+/* Copyright Notice                      */
+/* Copyright(c)2019 5G Range Consortium  */
+/* All rights Reserved                   */
+/*****************************************/
+/**
+@Arquive name : TransmissionProtocol.cpp
+@Classification : Transmission Protocol
+@
+@Last alteration : November 28th, 2019
+@Responsible : Eduardo Melao
+@Email : emelao@cpqd.com.br
+@Telephone extension : 7015
+@Version : v1.0
+
+Project : H2020 5G-Range
+
+Company : Centro de Pesquisa e Desenvolvimento em Telecomunicacoes (CPQD)
+Direction : Diretoria de Operações (DO)
+UA : 1230 - Centro de Competencia - Sistemas Embarcados
+
+@Description : This module acts as broker between L2 Core and upper and upper layers.
+*/
+
+#include "TransmissionProtocol.h"
+
+TransmissionProtocol::TransmissionProtocol(
+    L1L2Interface* _l1l2Interface,  //Object to send packets to L1
+    TunInterface* _tunInterface,    //Object to send packets to L3
+    bool _verbose)                  //Verbosity flag
+{
+    l1l2Interface = _l1l2Interface;
+    tunInterface = _tunInterface;
+    verbose = _verbose;
+}
+
+TransmissionProtocol::~TransmissionProtocol() {}
+
+bool 
+TransmissionProtocol::sendPackageToL1(
+    char* dataBuffer,       //Information Buffer
+    size_t dataSize,        //Size of information Buffer in Bytes
+    char* controlBuffer,    //Control information Buffer
+    size_t controlSize,     //Size of control information in Bytes
+    int port)               // Socket port to identify which socket to send information
+{
+    if(verbose) cout<<"[TransmissionProtocol] Sending packet to L1."<<endl;
+    return l1l2Interface->sendPdu((uint8_t*)dataBuffer, dataSize,(uint8_t*)controlBuffer, controlSize, port);
+}
+
+bool 
+TransmissionProtocol::sendPackageToL3(
+    char* buffer,   //Buffer where packet will be stored
+    size_t size)       //Size of information in Bytes
+{
+    if(verbose) cout<<"[TransmissionProtocol] Sending packet to L3."<<endl;
+    return tunInterface->writeTunInterface(buffer, size);
+}
